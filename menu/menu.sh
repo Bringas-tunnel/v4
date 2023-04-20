f17343d7089af9dd430c06d3087f12bdc6333d5e
Bringas-tunnel <131277468+Bringas-tunnel@users.noreply.github.com>
2023-04-21 01:35:01 +0700

    Update menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -275,19 +275,19 @@ echo -e "\e[33m Domain               \e[0m: \033[1;36m $domain ${NC}"
 echo -e "\e[33m Ip Vps               \e[0m: \033[1;36m $IPVPS ${NC}"	
 echo -e "${CYAN}╘━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╛\033[0m${NC}"
 echo -e "${CYAN}╒━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╕\033[0m${NC}"
-echo -e " \E[44;1;39m                 • SCRIPT MENU •                \E[0m"
+echo -e " \E[44;1;39m                 • MENU •                \E[0m"
 echo -e "${CYAN}╘━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╛\033[0m${NC}"
-echo -e " [\e[36m•1\e[0m] SSH Menu  [\e[36m•5\e[0m]\033[1;31m CEK AREK AKTIF SSH$NC"
-echo -e " [\e[36m•2\e[0m] v2ray Menu         [\e[36m•6\e[0m]\033[1;31m CEK AREK AKTIF XRAY$NC"
-echo -e " [\e[36m•3\e[0m] Vless Menu         [\e[36m•7\e[0m] Clear RAM Cache"
-echo -e " [\e[36m•4\e[0m] Trojan Go Menu     [\e[36m•8\e[0m] Trojan WSS Menu"                  
+echo -e " [\e[36m•1\e[0m] SSH           [\e[36m•5\e[0m]\033[1;31m CEK AREK AKTIF SSH$NC"
+echo -e " [\e[36m•2\e[0m] v2ray         [\e[36m•6\e[0m]\033[1;31m CEK AREK AKTIF XRAY$NC"
+echo -e " [\e[36m•3\e[0m] Vless         [\e[36m•7\e[0m] Clear RAM Cache"
+echo -e " [\e[36m•4\e[0m] Trojan Go     [\e[36m•8\e[0m] Trojan WSS"                  
 echo -e  "\e[36m╒════════════════════════════════════════════════╕\033[0m"
-echo -e " \E[44;1;39m                 Settings MENU                  \E[0m"
+echo -e " \E[44;1;39m                 • SETINGS •                \E[0m"
 echo -e "\e[36m╘════════════════════════════════════════════════╛\033[0m
  [\033[1;36m09\033[0m] Setting Menu [\033[1;32mPro${NC}] [\033[1;36m14\033[0m] \033[1;31mSEtting Pasword VPS${NC}
  [\033[1;36m10\033[0m] Gen cert SSL       [\033[1;36m15\033[0m] \033[1;33mSEtting Auto Reboot${NC}
  [\033[1;36m11\033[0m] Cek Status \033[1;32mRUNNING${NC} [\033[1;36m16\033[0m] Cek Pkt bulanan
- [\033[1;36m12\033[0m] Reboot VPS \033[1;36mX-Ray${NC}   [\033[1;36m17\033[0m] Cek Pkt \033[1;33mHarian${NC}
+ [\033[1;36m12\033[0m] Reboot  VPS \033[1;36mX-Ray${NC}   [\033[1;36m17\033[0m] Cek Pkt \033[1;33mHarian${NC}
  [\033[1;36m13\033[0m] Restart VPS        [\033[1;36m18\033[0m] CEK INFO \033[1;33m VPS${NC}
 "
 echo -e   " \033[1;33m Press x or [ Ctrl+C ] • To-Exit-Script ${NC}"
