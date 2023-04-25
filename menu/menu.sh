c65245c897afcdaf6ba35db2e06b67bc945935bb
Bringas-tunnel <131277468+Bringas-tunnel@users.noreply.github.com>
2023-04-25 16:36:22 +0700

    Update menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -284,11 +284,11 @@ echo -e " [\e[36m•4\e[0m] Trojan Go     [\e[36m•8\e[0m] Trojan WSS"
 echo -e  "\e[36m╒════════════════════════════════════════════════╕\033[0m"
 echo -e " \E[44;1;36m                 • SETINGS •                \E[0m"
 echo -e "\e[36m╘════════════════════════════════════════════════╛\033[0m
- [\033[1;36m09\033[0m] Setting Menu${NC}  [\033[1;32m14\033[0m] SEtting Pasword VPS${NC}
- [\033[1;36m10\033[0m] Gen cert SSL       [\033[1;36m15\033[0m] SEtting Auto Reboot${NC}
+ [\033[1;36m09\033[0m] Setting Menu${NC}  [\033[1;36m14\033[0m] SEtting Pasword VPS${NC}
+ [\033[1;36m10\033[0m] Gen cert SSL   [\033[1;36m15\033[0m] SEtting Auto Reboot${NC}
  [\033[1;36m11\033[0m] Cek RUNNING${NC}   [\033[1;36m16\033[0m] Cek Pkt bulanan
  [\033[1;36m12\033[0m] Reboot X-Ray${NC}  [\033[1;36m17\033[0m] Cek Pkt Harian${NC}
- [\033[1;36m13\033[0m] Restart VPS        [\033[1;36m18\033[0m] CEK INFO VPS${NC}
+ [\033[1;36m13\033[0m] Restart VPS    [\033[1;36m18\033[0m] CEK INFO VPS${NC}
 "
 echo -e   " \033[1;33m Press x or [ Ctrl+C ] • To-Exit-Script ${NC}"
 echo -e  "\e[36m╒════════════════════════════════════════════════╕\033[0m"
