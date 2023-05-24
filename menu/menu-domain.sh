e6edd4fde3bdcdbb7ec0c719e3f35809b420204e
Bringas-tunnel <131277468+Bringas-tunnel@users.noreply.github.com>
2023-05-24 18:43:35 +0700

    Update menu-domain.sh

--- menu/menu-domain.sh
+++ menu/menu-domain.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
-MYIP=$(wget -qO- ipinfo.io/ip);
-echo "Checking VPS"
-
+MYIP=$(wget -qO- ipinfo.io/ip)
 clear 
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e "\E[40;1;37m        • DOMAIN MENU •            \E[0m"
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e "" 
 echo -e " [\e[36m•1\e[0m] CHANGE DOMAIN VPS"
-echo -e " [\e[36m•2\e[0m] Renew Certificate DOMAIN"
-echo -e ""
+echo -e " [\e[36m•2\e[0m] Renew Cert DOMAIN"
 echo -e " [\e[31m•0\e[0m] \e[31mBACK TO MENU\033[0m"
-echo -e   ""
-echo -e   "Press x or [ Ctrl+C ] • To-Exit"
-echo -e   ""
+echo -e ""
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e ""
 read -p " Select menu : " opt
@@ -23,5 +18,5 @@ case $opt in
 2) clear ; certv2ray ;;
 0) clear ; menu ;;
 x) exit ;;
-*) echo "Anda salah tekan" ; sleep 1 ; menu-domain ;;
+*) echo "salah pencet bro" ; sleep 1 ; menu-domain ;;
 esac
