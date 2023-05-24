83491f6fc14b3e0b216192fe7ca1f5cb84d3df3e
Bringas-tunnel <131277468+Bringas-tunnel@users.noreply.github.com>
2023-05-24 19:37:33 +0700

    Update menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -262,42 +262,42 @@ else
     fi
 
 clear
-echo -e "${CYAN}╒━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╕\033[0m${NC}"
-echo -e " \E[44;1;33m           ⇱ BRINGAS FAMILY ⇲           \E[0m"
-echo -e "${CYAN}╘━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╛\033[0m${NC}"
+echo -e "${CYAN}╒━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╕\033[0m${NC}"
+echo -e " \E[44;1;33m           ⇱ BRINGAS FAMILY ⇲             \E[0m"
+echo -e "${CYAN}╘━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╛\033[0m${NC}"
 echo -e "    [ SSH : $status_ws_epro ] [ X-RAY : $status_ss ] [ NGINX : $status_nginx ]"
-echo -e "${CYAN}╒━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╕\033[0m${NC}"
+echo -e "${CYAN}╒━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╕\033[0m${NC}"
 echo -e "\e[37m OPERATING     \e[0m: \033[1;32m "`hostnamectl | grep "Operating System" | cut -d ' ' -f5-`	${NC}
 echo -e "\e[37m TOTAL RAM     \e[0m: \033[1;32m $tram MB ${NC}"
 echo -e "\e[37m SYSTEM        \e[0m: \033[1;32m $uptime ${NC}"
 echo -e "\e[37m SERVER        \e[0m: \033[1;32m $ISP ${NC}"
 echo -e "\e[37m DOMAIN        \e[0m: \033[1;32m $domain ${NC}"	
 echo -e "\e[37m IP VPS        \e[0m: \033[1;32m $IPVPS ${NC}"	
-echo -e "${CYAN}╘━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╛\033[0m${NC}"
-echo -e "${CYAN}╒━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╕\033[0m${NC}"
+echo -e "${CYAN}╘━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╛\033[0m${NC}"
+echo -e "${CYAN}╒━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╕\033[0m${NC}"
 echo -e " \E[44;1;33m                 • MENU •                \E[0m"
-echo -e "${CYAN}╘━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╛\033[0m${NC}"
+echo -e "${CYAN}╘━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╛\033[0m${NC}"
 echo -e " [\e[33m•1\e[0m] ssh"
 echo -e " [\e[33m•2\e[0m] vmess"      
 echo -e " [\e[33m•3\e[0m] vless"
 echo -e " [\e[33m•4\e[0m] trojan-GO"
-echo -e " [\e[33m•5\e[0m] ssh Active$NC"
-echo -e " [\e[33m•6\e[0m] vmes Active$NC"
-echo -e " [\e[33m•7\e[0m] clear Cache"
-echo -e " [\e[33m•8\e[0m] trojan-WS"
-echo -e "\e[36m╒════════════════════════════════════════════════╕\033[0m"
+echo -e " [\e[33m•5\e[0m] trojan-WS"
+echo -e " [\e[33m•6\e[0m] clear Cache"
+echo -e " [\e[33m•7\e[0m] vmes Active$NC"
+echo -e " [\e[33m•8\e[0m] ssh Active$NC"
+echo -e "\e[36m╒══════════════════════════════════════════════╕\033[0m"
 echo -e " \E[44;1;33m                 • SETTINGS •                \E[0m"
-echo -e "\e[36m╘════════════════════════════════════════════════╛\033[0m
+echo -e "\e[36m╘══════════════════════════════════════════════╛\033[0m
  [\033[1;36m09\033[0m] Setting Menu${NC}  [\033[1;36m14\033[0m] SEtting Pasword VPS${NC}
  [\033[1;36m10\033[0m] Gen cert SSL  [\033[1;36m15\033[0m] SEtting Auto Reboot${NC}
- [\033[1;36m11\033[0m] cek Running${NC}   [\033[1;36m16\033[0m] Cek Pkt bulanan
+ [\033[1;36m11\033[0m] Cek Running${NC}   [\033[1;36m16\033[0m] Cek Pkt Bulanan
  [\033[1;36m12\033[0m] Reboot X-Ray${NC}  [\033[1;36m17\033[0m] Cek Pkt Harian${NC}
- [\033[1;36m13\033[0m] Restart VPS   [\033[1;36m18\033[0m] CEK INFO VPS${NC}"
-echo -e "\e[36m╒════════════════════════════════════════════════╕\033[0m"
+ [\033[1;36m13\033[0m] Restart VPS   [\033[1;36m18\033[0m] Cek VPS${NC}"
+echo -e "\e[36m╒══════════════════════════════════════════════╕\033[0m"
 echo -e "\e[36m│  Client   =$NC \033[1;32m $Name ${NC}"
 echo -e "\e[36m│  User     =${NC} \033[1;32m SC by: BRINGAS ${NC}"
 echo -e "\e[36m│  Exp      =$NC \033[1;32m $Exp Day ${NC}"
-echo -e "\e[36m╘════════════════════════════════════════════════╛\033[0m"
+echo -e "\e[36m╘══════════════════════════════════════════════╛\033[0m"
 echo -e   ""
 read -p " Select menu :  "  opt
 echo -e   ""
@@ -306,10 +306,10 @@ case $opt in
 2) clear ; menu-vmess ;;
 3) clear ; menu-vless ;;
 4) clear ; menu-trgo ;;
-5) clear ; cek ;;
-6) clear ; cek-ws ;;
-7) clear ; clearcache ;;
-8) clear ; menu-trojan ;;
+5) clear ; menu-trojan ;;
+6) clear ; clearcache ;;
+7) clear ; cek-ws ;;
+8) clear ; cek ;;
 9) clear ; menu-set ;;
 10) clear ; certv2ray ;;
 11) clear ; status ;;
