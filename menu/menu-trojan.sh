1e9ac8d997a113428efaa135ce65348194f56761
Bringas-tunnel <131277468+Bringas-tunnel@users.noreply.github.com>
2023-05-25 22:02:33 +0700

    Update menu-trojan.sh

--- menu/menu-trojan.sh
+++ menu/menu-trojan.sh
@@ -66,8 +66,6 @@ exit 0
 fi
 
 MYIP=$(wget -qO- ipinfo.io/ip);
-echo "Checking VPS"
-
 clear 
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e "\E[40;1;37m      • MENU TROJAN-WSS •          \E[0m"
@@ -78,14 +76,11 @@ echo -e " [\e[36m•2\e[0m] Buat Akun Trial "
 echo -e " [\e[36m•3\e[0m] Ubah Akun Trojan "
 echo -e " [\e[36m•4\e[0m] Hapus Akun Trojan "
 echo -e " [\e[36m•5\e[0m] Cek user Login Trojan "
-echo -e ""
 echo -e " [\e[31m•0\e[0m] \e[31mBACK TO MENU\033[0m"
-echo -e   ""
-echo -e   "Press x or [ Ctrl+C ] • To-Exit"
 echo ""
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e ""
-read -p " Select menu : " opt
+read -p " Select =>  " opt
 echo -e ""
 case $opt in
 1) clear ; add-tr ;;
