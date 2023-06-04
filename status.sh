0a6c1b0b7e0b4e7d8fb164399e75636854a81418
Bringas-tunnel <131277468+Bringas-tunnel@users.noreply.github.com>
2023-06-04 07:31:40 +0700

    Update status.sh

--- status.sh
+++ status.sh
@@ -132,8 +132,8 @@ clear;clear;clear
 
 echo -e "${YELLOW}----------------------------------------------------------${NC}"
 echo -e "                ${YELLOW}(${NC}${GREEN} STATUS SERVICE INFORMATION ${NC}${YELLOW})${NC}"
-echo -e "                  OWNER : ${GREEN} BRINGASZ ${NC}${YELLOW}${NC} ${GREEN}TUNNEL ${NC}${YELLOW}${NC}"
-echo -e "       © Copyright By Bringas-tunnel ${YELLOW}(${NC} 2022-2023 ${YELLOW})${NC}"
+echo -e "                  CREATOR : ${GREEN} BRINGAS ${NC}${YELLOW}${NC} ${GREEN}TUNNEL ${NC}${YELLOW}${NC}"
+echo -e "       © Copyright By Bringas-tunnel ${YELLOW}(${NC} 2014-2023 ${YELLOW})${NC}"
 echo -e "${YELLOW}----------------------------------------------------------${NC}"
 echo ""
 echo -e "${RED_BG}                     Sytem Information                    ${NC}"
