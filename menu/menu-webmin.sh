f815b2d8bb733440dd341344c1a2e94eae17f026
Bringas-tunnel <131277468+Bringas-tunnel@users.noreply.github.com>
2023-06-04 07:34:53 +0700

    Update menu-webmin.sh

--- menu/menu-webmin.sh
+++ menu/menu-webmin.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 MYIP=$(wget -qO- ipinfo.io/ip);
-echo "Checking VPS"
-
+echo "wait................
 clear
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
 Info="${Green_font_prefix}[Installed]${Font_color_suffix}"
