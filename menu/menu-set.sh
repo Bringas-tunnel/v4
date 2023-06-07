ce4648cee4b3788bbc620cd945abb690d24ec948
Bringas-tunnel <131277468+Bringas-tunnel@users.noreply.github.com>
2023-06-07 09:34:06 +0700

    Update menu-set.sh

--- menu/menu-set.sh
+++ menu/menu-set.sh
@@ -19,7 +19,7 @@ echo -e " [\e[31m•0\e[0m] \e[31mBACK TO MENU\033[0m"
 echo -e   ""
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e ""
-read -p " Select " opt
+read -p " pilih ➡  " opt
 echo -e ""
 case $opt in
 1) clear ; menu-domain ; exit ;;
