0e51d252d40558061d17e1229068efa17817e276
Bringas-tunnel <131277468+Bringas-tunnel@users.noreply.github.com>
2023-06-08 15:02:55 +0700

    Update menu-trgo.sh

--- menu/menu-trgo.sh
+++ menu/menu-trgo.sh
@@ -66,7 +66,7 @@ exit 0
 fi
 MYIP=$(wget -qO- ipinfo.io/ip);
 clear 
-echo -e "\e[1;37m𝙏𝙍𝙊𝙅𝘼𝙉-𝙜𝙤 𝙈𝙀𝙉𝙐\033[0m"
+echo -e "\e[1;37m𝙏𝙍𝙊𝙅𝘼𝙉-𝙜𝙤 𝙈𝙀𝙉𝙐 \033[0m"
 echo -e " "
 echo -e "         [\e[1;37m•1\e[0m] \e[1;36mCreate TrojanGo\033[0m"
 echo -e "         [\e[1;37m•2\e[0m] \e[1;36mCreate Trial\033[0m"
@@ -74,7 +74,7 @@ echo -e "         [\e[1;37m•3\e[0m] \e[1;36mXtend TrojanGo\033[0m"
 echo -e "         [\e[1;37m•4\e[0m] \e[1;36mDelete Akun Trojan-Go\033[0m"
 echo -e "         [\e[1;37m•5\e[0m] \e[1;36mCek Login Trojan-Go\033[0m"
 echo -e "         [\e[1;33m•0\e[0m] \e[1;33mBack To Menu\033[0m"
-echo -e "         ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | lolcat
+echo -e "         ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | lolcat
 echo -e ""
 read -p " >>>   " opt
 echo -e ""
