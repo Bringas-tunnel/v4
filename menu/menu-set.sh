76e78dbd8156fea0d455361aeb356eea45a6cd82
Bringas-tunnel <131277468+Bringas-tunnel@users.noreply.github.com>
2023-06-08 07:38:01 +0700

    Update menu-set.sh

--- menu/menu-set.sh
+++ menu/menu-set.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 MYIP=$(wget -qO- ipinfo.io/ip);
 clear 
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\E[40;1;37m          • SYSTEM MENU •          \E[0m"
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e " [\e[36m•1\e[0m] \e[36mUbah Domain vps\033[0m"
-echo -e " [\e[36m•2\e[0m] \e[36mUbah Port\033[0m"
-echo -e " [\e[36m•3\e[0m] \e[36mUbah banner\033[0m"
-echo -e " [\e[36m•4\e[0m] \e[36mRestart service\033[0m"
-echo -e " [\e[36m•5\e[0m] \e[36mCek bandwith\033[0m"
-echo -e " [\e[36m•6\e[0m] \e[36mAtur waktu riset\033[0m"
-echo -e " [\e[36m•7\e[0m] \e[36mSpeedtest vps\033[0m"
-echo -e " [\e[36m•8\e[0m] \e[36mWebmin menu\033[0m"
-echo -e " [\e[36m•9\e[0m] \e[36mTentang Script\033[0m"
-echo -e " [\e[31m•0\e[0m] \e[31mBACK TO MENU\033[0m"
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e                     "SYSTEM MENU" | lolcat
+echo -e " "
+echo -e "         [\e[36m•1\e[0m] \e[1;36mUbah Domain vps\033[0m"
+echo -e "         [\e[36m•2\e[0m] \e[1;36mUbah Port\033[0m"
+echo -e "         [\e[36m•3\e[0m] \e[1;36mUbah banner\033[0m"
+echo -e "         [\e[36m•4\e[0m] \e[1;36mRestart service\033[0m"
+echo -e "         [\e[36m•5\e[0m] \e[1;36mCek bandwith\033[0m"
+echo -e "         [\e[36m•6\e[0m] \e[1;36mAtur waktu riset\033[0m"
+echo -e "         [\e[36m•7\e[0m] \e[1;36mSpeedtest vps\033[0m"
+echo -e "         [\e[36m•8\e[0m] \e[1;36mWebmin menu\033[0m"
+echo -e "         [\e[36m•9\e[0m] \e[1;36mTentang Script\033[0m"
+echo -e "         [\e[31m•0\e[0m] \e[1;33mBACK TO MENU\033[0m"
+echo -e "       ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | lolcat
 echo -e ""
 read -p " >>>>>>>   " opt
 echo -e ""
