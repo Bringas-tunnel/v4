44b18345ba23167dd56a8d96964e89d999449587
Bringas-tunnel <131277468+Bringas-tunnel@users.noreply.github.com>
2023-06-08 08:39:08 +0700

    Update menu-set.sh

--- menu/menu-set.sh
+++ menu/menu-set.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 MYIP=$(wget -qO- ipinfo.io/ip);
 clear 
-echo -e                     "SYSTEM MENU" | lolcat
+echo -e "\e[1;37mSYSTEM MENU\033[0m"
 echo -e " "
-echo -e "         [\e[36m•1\e[0m] \e[1;36mUbah Domain vps\033[0m"
-echo -e "         [\e[36m•2\e[0m] \e[1;36mUbah Port\033[0m"
-echo -e "         [\e[36m•3\e[0m] \e[1;36mUbah banner\033[0m"
-echo -e "         [\e[36m•4\e[0m] \e[1;36mRestart service\033[0m"
-echo -e "         [\e[36m•5\e[0m] \e[1;36mCek bandwith\033[0m"
-echo -e "         [\e[36m•6\e[0m] \e[1;36mAtur waktu riset\033[0m"
-echo -e "         [\e[36m•7\e[0m] \e[1;36mSpeedtest vps\033[0m"
-echo -e "         [\e[36m•8\e[0m] \e[1;36mWebmin menu\033[0m"
-echo -e "         [\e[36m•9\e[0m] \e[1;36mTentang Script\033[0m"
-echo -e "         [\e[31m•0\e[0m] \e[1;33mBACK TO MENU\033[0m"
+echo -e "         [\e[1;37m•1\e[0m] \e[1;36mUbah Domain vps\033[0m"
+echo -e "         [\e[1;37m•2\e[0m] \e[1;36mUbah Port\033[0m"
+echo -e "         [\e[1;37m•3\e[0m] \e[1;36mUbah banner\033[0m"
+echo -e "         [\e[1;37m•4\e[0m] \e[1;36mRestart service\033[0m"
+echo -e "         [\e[1;37m•5\e[0m] \e[1;36mCek bandwith\033[0m"
+echo -e "         [\e[1;37m•6\e[0m] \e[1;36mAtur waktu riset\033[0m"
+echo -e "         [\e[1;37m•7\e[0m] \e[1;36mSpeedtest vps\033[0m"
+echo -e "         [\e[1;37m•8\e[0m] \e[1;36mWebmin menu\033[0m"
+echo -e "         [\e[1;37m•9\e[0m] \e[1;36mTentang Script\033[0m"
+echo -e "         [\e[1;33m•0\e[0m] \e[1;33mBack To Menu\033[0m"
 echo -e "       ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | lolcat
 echo -e ""
-read -p " >>>>>>>   " opt
+read -p " >>>   " opt
 echo -e ""
 case $opt in
 1) clear ; menu-domain ; exit ;;
