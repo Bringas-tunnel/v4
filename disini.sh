185481cbe1a070f6c9c4f94d22cdba56b03dd6e2
Bringas-tunnel <131277468+Bringas-tunnel@users.noreply.github.com>
2023-06-09 22:01:37 +0700

    Update disini.sh

--- disini.sh
+++ disini.sh
@@ -1,1 +1,6 @@
+#!/bin/bash
+dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
+###########- COLOR CODE -##############
+echo -e " [INFO] Downloading Update File"
 wget -q -O /usr/bin/menu "https://raw.githubusercontent.com/Bringas-tunnel/v4/main/menu/menu.sh" && chmod +x /usr/bin/menu
