a6e5ae9b40474d6ca8ee4f99c2721c45e911dcd6
Bringas-tunnel <131277468+Bringas-tunnel@users.noreply.github.com>
2023-06-10 12:36:33 +0700

    Update hapus.sh

--- ssh/hapus.sh
+++ ssh/hapus.sh
@@ -6,11 +6,9 @@ biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 
 
 clear
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\E[40;1;37m               DELETE USER                \E[0m"
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"  
-echo ""
-read -p "Username SSH to Delete : " Pengguna
+echo -e "\e[1;37mDELETE USER\033[0m"
+echo -e ""  
+read -p              "Delete username >  " Pengguna
 
 if getent passwd $Pengguna > /dev/null 2>&1; then
         userdel $Pengguna > /dev/null 2>&1
