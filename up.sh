52dff244f331106860e672f1c15aa5d2d9a5aecc
Bringas-tunnel <131277468+Bringas-tunnel@users.noreply.github.com>
2023-06-10 23:34:45 +0700

    Update up.sh

--- up.sh
+++ up.sh
@@ -8,13 +8,14 @@ sleep 2
 rm -rf menu
 rm -rf menu-trial
 rm -rf menu-vmess
-rm -rf menu-vless
+rm -rf menu-vlesss
 rm -rf running
 rm -rf clearcache
 rm -rf menu-trgo
 rm -rf menu-trojan
-rm -rf fix
+rm -rf menu-sshh
 rm -rf menu-ssh
+rm -rf menu-sshh
 rm -rf usernew
 rm -rf trial
 rm -rf renew
@@ -24,6 +25,7 @@ rm -rf member
 rm -rf delete
 rm -rf autokill
 rm -rf ceklim
+rm -rf autokilll
 rm -rf tendang
 rm -rf menu-set
 rm -rf menu-domain
@@ -51,58 +53,56 @@ rm -rf up
 # download script
 cd /usr/bin
 # menu
-wget -O menu "https://raw.githubusercontent.com/Andyyuda/ver3/main/menu/menu.sh"
-wget -O menu-trial "https://raw.githubusercontent.com/Andyyuda/ver3/main/menu/menu-trial.sh"
-wget -O menu-vmess "https://raw.githubusercontent.com/Andyyuda/ver3/main/menu/menu-vmess.sh"
-wget -O menu-vless "https://raw.githubusercontent.com/Andyyuda/ver3/main/menu/menu-vless.sh"
-wget -O running "https://raw.githubusercontent.com/artanodrop/v4/main/menu/running.sh"
-wget -O clearcache "https://raw.githubusercontent.com/Andyyuda/ver3/main/menu/clearcache.sh"
-wget -O menu-trgo "https://raw.githubusercontent.com/artanodrop/v4/main/menu/menu-trgo.sh"
-wget -O menu-trojan "https://raw.githubusercontent.com/Andyyuda/ver3/main/menu/menu-trojan.sh"
-wget -O fix "https://raw.githubusercontent.com/Andyyuda/ver3/main/menu/cf.sh"
+wget -O menu "https://raw.githubusercontent.com/Bringas-tunnel/v4/main/menu/menu.sh"
+wget -O menu-trial "https://raw.githubusercontent.com/Fikripps/Ver3/main/menu/menu-trial.sh"
+wget -O menu-vmess "https://raw.githubusercontent.com/Bringas-tunnel/v4/main/menu/menu-vmess.sh"
+wget -O menu-vless "https://raw.githubusercontent.com/Bringas-tunnel/v4/main/menu-vless.sh"
+wget -O running "https://raw.githubusercontent.com/Bringas-tunnel/v4/main/menu/running.sh"
+wget -O clearcache "https://raw.githubusercontent.com/Fikripps/Ver3/main/menu/clearcache.sh"
+wget -O menu-trgo "https://raw.githubusercontent.com/Bringas-tunnel/v4/main/menu/menu-trgo.sh"
+wget -O menu-trojan "https://raw.githubusercontent.com/Bringas-tunnel/v4/main/menu/menu-trojan.sh"
 
 # menu ssh ovpn
-wget -O menu-ssh "https://raw.githubusercontent.com/Andyyuda/ver3/main/menu/menu-ssh.sh"
-wget -O usernew "https://raw.githubusercontent.com/Andyyuda/ver3/main/ssh/usernew.sh"
-wget -O trial "https://raw.githubusercontent.com/Andyyuda/ver3/main/ssh/trial.sh"
-wget -O renew "https://raw.githubusercontent.com/artanodrop/v4/main/ssh/renew.sh"
-wget -O hapus "https://raw.githubusercontent.com/artanodrop/v4/main/ssh/hapus.sh"
-wget -O cek "https://raw.githubusercontent.com/artanodrop/v4/main/ssh/cek.sh"
-wget -O member "https://raw.githubusercontent.com/artanodrop/v4/main/ssh/member.sh"
-wget -O delete "https://raw.githubusercontent.com/artanodrop/v4/main/ssh/delete.sh"
-wget -O autokill "https://raw.githubusercontent.com/artanodrop/v4/main/ssh/autokill.sh"
-wget -O ceklim "https://raw.githubusercontent.com/artanodrop/v4/main/ssh/ceklim.sh"
-wget -O tendang "https://raw.githubusercontent.com/artanodrop/v4/main/ssh/tendang.sh"
+wget -O menu-sshh "https://raw.githubusercontent.com/Bringas-tunnel/v4/main/menu/menu-sshh.sh"
+wget -O usernew "https://raw.githubusercontent.com/Bringas-tunnel/v4/main/ssh/usernew.sh"
+wget -O trial "https://raw.githubusercontent.com/Bringas-tunnel/v4/main/ssh/trial.sh"
+wget -O renew "https://raw.githubusercontent.com/Bringas-tunnel/v4/main/ssh/renew.sh"
+wget -O hapus "https://raw.githubusercontent.com/Bringas-tunnel/v4/main/ssh/hapus.sh"
+wget -O cek "https://raw.githubusercontent.com/Bringas-tunnel/v4/main/ssh/cek.sh"
+wget -O member "https://raw.githubusercontent.com/Bringas-tunnel/v4/main/ssh/member.sh"
+wget -O delete "https://raw.githubusercontent.com/Bringas-tunnel/v4/main/ssh/delete.sh"
+wget -O autokilll "https://raw.githubusercontent.com/Bringas-tunnel/v4/main/ssh/autokilll.sh"
+wget -O ceklim "https://raw.githubusercontent.com/Bringas-tunnel/v4/main/ssh/ceklim.sh"
+wget -O tendang "https://raw.githubusercontent.com/Bringas-tunnel/v4/main/ssh/tendang.sh"
 
 # menu system
-wget -O menu-set "https://raw.githubusercontent.com/Andyyuda/ver3/main/menu/menu-set.sh"
-wget -O menu-domain "https://raw.githubusercontent.com/artanodrop/v4/main/menu/menu-domain.sh"
-wget -O add-host "https://raw.githubusercontent.com/artanodrop/v4/main/ssh/add-host.sh"
-wget -O port-change "https://raw.githubusercontent.com/artanodrop/v4/main/port/port-change.sh"
-wget -O certv2ray "https://raw.githubusercontent.com/artanodrop/v4/main/xray/certv2ray.sh"
-wget -O menu-webmin "https://raw.githubusercontent.com/artanodrop/v4/main/menu/menu-webmin.sh"
-wget -O speedtest "https://raw.githubusercontent.com/artanodrop/v4/main/ssh/speedtest_cli.py"
-wget -O about "https://raw.githubusercontent.com/Andyyuda/ver3/main/menu/about.sh"
-wget -O auto-reboot "https://raw.githubusercontent.com/artanodrop/v4/main/menu/auto-reboot.sh"
-wget -O restart "https://raw.githubusercontent.com/artanodrop/v4/main/menu/restart.sh"
-wget -O bw "https://raw.githubusercontent.com/artanodrop/v4/main/menu/bw.sh"
+wget -O menu-set "https://raw.githubusercontent.com/Bringas-tunnel/v4/main/menu/menu-set.sh"
+wget -O menu-domain "https://raw.githubusercontent.com/Bringas-tunnel/v4/main/menu/menu-domain.sh"
+wget -O add-host "https://raw.githubusercontent.com/Bringas-tunnel/v4/main/ssh/add-host.sh"
+wget -O port-change "https://raw.githubusercontent.com/Bringas-tunnel/v4/main/port/port-change.sh"
+wget -O certv2ray "https://raw.githubusercontent.com/Bringas-tunnel/v4/main/xray/certv2ray.sh"
+wget -O menu-webmin "https://raw.githubusercontent.com/Bringas-tunnel/v4/main/menu/menu-webmin.sh"
+wget -O speedtest "https://raw.githubusercontent.com/Bringas-tunnel/v4/main/ssh/speedtest_cli.py"
+wget -O about "https://raw.githubusercontent.com/Fikripps/Ver3/main/menu/about.sh"
+wget -O auto-reboot "https://raw.githubusercontent.com/Bringas-tunnel/v4/main/menu/auto-reboot.sh"
+wget -O restarts "https://raw.githubusercontent.com/Bringas-tunnel/v4/main/menu/restarts.sh"
+wget -O bw "https://raw.githubusercontent.com/Bringas-tunnel/v4/main/menu/bw.sh"
 
 # change port
-wget -O port-ssl "https://raw.githubusercontent.com/artanodrop/v4/main/port/port-ssl.sh"
-wget -O port-ovpn "https://raw.githubusercontent.com/artanodrop/v4/main/port/port-ovpn.sh"
+wget -O port-ssl "https://raw.githubusercontent.com/Bringas-tunnel/v4/main/port/port-ssl.sh"
+wget -O port-ovpn "https://raw.githubusercontent.com/Bringas-tunnel/v4/main/port/port-ovpn.sh"
 
 
-wget -O xp "https://raw.githubusercontent.com/artanodrop/v4/main/ssh/xp.sh"
-wget -O acs-set "https://raw.githubusercontent.com/artanodrop/v4/main/acs-set.sh"
+wget -O xp "https://raw.githubusercontent.com/Bringas-tunnel/v4/main/ssh/xp.sh"
+wget -O acs-set "https://raw.githubusercontent.com/Bringas-tunnel/v4/main/acs-set.sh"
 
-wget -O sshws "https://raw.githubusercontent.com/artanodrop/v4/main/ssh/sshws.sh"
-wget -O status "https://raw.githubusercontent.com/artanodrop/v4/main/status.sh"
-wget -O menu-backup "https://raw.githubusercontent.com/Andyyuda/ver3/main/backup/menu-backup.sh"
-wget -O backup "https://raw.githubusercontent.com/Andyyuda/ver3/main/backup/backup.sh"
-wget -O restore "https://raw.githubusercontent.com/Andyyuda/ver3/main/backup/restore.sh"
+wget -O sshws "https://raw.githubusercontent.com/Bringas-tunnel/v4/main/ssh/sshws.sh"
+wget -O status "https://raw.githubusercontent.com/Bringas-tunnel/v4/main/status.sh"
+wget -O menu-bckp "https://raw.githubusercontent.com/Fikripps/Ver3/main/menu/menu-bckp.sh"
+wget -O backup "https://raw.githubusercontent.com/Bringas-tunnel/v4/main/backup/backup.sh"
+wget -O restore "https://raw.githubusercontent.com/Bringas-tunnel/v4/main/backup/restore.sh"
 
-wget -O jam "https://raw.githubusercontent.com/artanodrop/v4/main/jam.sh"
-wget https://raw.githubusercontent.com/Andyvpn/Autoscript-by-azi/main/autoscript-ssh-slowdns-main/slowdns.sh && chmod +x slowdns.sh && ./slowdns.sh
+wget -O jam "https://raw.githubusercontent.com/Bringas-tunnel/v4/main/jam.sh"
 
 chmod +x menu
 chmod +x menu-trial
@@ -112,10 +112,8 @@ chmod +x running
 chmod +x clearcache
 chmod +x menu-trgo
 chmod +x menu-trojan
-chmod +x fix
-chmod +x up
 
-chmod +x menu-ssh
+chmod +x menu-sshh
 chmod +x usernew
 chmod +x trial
 chmod +x renew
@@ -123,7 +121,7 @@ chmod +x hapus
 chmod +x cek
 chmod +x member
 chmod +x delete
-chmod +x autokill
+chmod +x autokilll
 chmod +x ceklim
 chmod +x tendang
 
@@ -136,7 +134,7 @@ chmod +x menu-webmin
 chmod +x speedtest
 chmod +x about
 chmod +x auto-reboot
-chmod +x restart
+chmod +x restarts
 chmod +x bw
 
 chmod +x port-ssl
@@ -146,7 +144,7 @@ chmod +x xp
 chmod +x acs-set
 chmod +x sshws
 chmod +x status
-chmod +x menu-backup
+chmod +x menu-bckp
 chmod +x backup
 chmod +x restore
 chmod +x jam
