11c451bc7502bda153719c1234cdbcb0c839be4b
Bringas-tunnel <131277468+Bringas-tunnel@users.noreply.github.com>
2023-06-12 13:21:53 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -12,9 +12,9 @@ rm -rf restore
 
 wget https://raw.githubusercontent.com/Bringas-tunnel/v4/main/backup/set-br.sh &&  chmod +x set-br.sh && ./set-br.sh
 wget -q -O /usr/bin/menu "https://raw.githubusercontent.com/Bringas-tunnel/v4/main/menu/menu.sh" && chmod +x /usr/bin/menu
-wget -q -O /usr/bin/menu-backup "https://raw.githubusercontent.com/Andyyuda/ver3/main/backup/menu-backup.sh" && chmod +x /usr/bin/menu-backup
-wget -q -O /usr/bin/backup "https://raw.githubusercontent.com/Andyyuda/ver3/main/backup/backup.sh" && chmod +x /usr/bin/backup
-wget -q -O /usr/bin/restore "https://raw.githubusercontent.com/Andyyuda/ver3/main/backup/restore.sh" && chmod +x /usr/bin/restore
+wget -q -O /usr/bin/menu-backup "https://raw.githubusercontent.com/Bringas-tunnel/v4/main/backup/menu-backup.sh" && chmod +x /usr/bin/menu-backup
+wget -q -O /usr/bin/backup "https://raw.githubusercontent.com/Bringas-tunnel/v4/main/backup/backup.sh" && chmod +x /usr/bin/backup
+wget -q -O /usr/bin/restore "https://raw.githubusercontent.com/Bringas-tunnel/v4/main/backup/restore.sh" && chmod +x /usr/bin/restore
 echo -e " [INFO] Update Successfully"
 sleep 2
 exit
