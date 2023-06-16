62aa781be1cae11e8da5ab24085a49702b7b98b3
Bringas-tunnel <131277468+Bringas-tunnel@users.noreply.github.com>
2023-06-16 17:06:33 +0700

    Update menu-domain.sh

--- menu/menu-domain.sh
+++ menu/menu-domain.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 MYIP=$(wget -qO- ipinfo.io/ip)
+green_background="\033[42;37m"
+NC="\033[0m"
+red_background="\033[41;37m"
+wh="\e[1;37m" # PUTIH
+cy="\e[1;36m" # CYAN
+ye="\e[1;33m" # KUNING
 clear 
-echo -e "\e[1;37mDOMAIN MENU\033[0m"
+echo -e "${red_background}DOMAIN MENU${NC}"
 echo -e ""
-echo -e "      \e[1;37m[•1]\e[0m] \e[1;36m𝗚𝗮𝗻𝘁𝗶 𝗗𝗼𝗺𝗮𝗶𝗻\033[0m"
-echo -e "      \e[1;37m[•2]\e[0m] \e[1;36m𝗥𝗲𝗻𝗲𝘄 𝗰𝗲𝗿𝘁 𝗱𝗼𝗺𝗮𝗶𝗻\033[0m"
-echo -e "      \e[1;33m[•0]\e[0m] \e[1;33mBack To Menu\033[0m"
-echo -e "     ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | lolcat
-echo -e ""
+echo -e "        ${wh}[•1]${NC} ${cy}𝗚𝗮𝗻𝘁𝗶 𝗗𝗼𝗺𝗮𝗶𝗻 ${Nc}"
+echo -e "        ${wh}[•2]${NC} ${cy}𝗥𝗲𝗻𝗲𝘄 𝗰𝗲𝗿𝘁 𝗱𝗼𝗺𝗮𝗶𝗻 ${NC}"
+echo -e "        ${ye}[•0]${NC} ${ye}Back To Menu${NC}"
+echo -e "${ye}"
 read -p " >>>   " opt
 echo -e ""
 case $opt in
